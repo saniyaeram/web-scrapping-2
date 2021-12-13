ec117115e1b4ad553352a5eabec599dcb931fe6b
saniyaeram <53005024+saniyaeram@users.noreply.github.com>
2021-12-13 14:14:49 +0530

    Update scrapper_2.py

--- scrapper_2.py
+++ scrapper_2.py
@@ -39,31 +39,8 @@ def scrape():
         browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
         print(f"{i} page done 1")
 def scrape_more_data(hyperlink):
-    try:
-        page = requests.get(hyperlink)
-        soup = BeautifulSoup(page.content, "html.parser")
-        temp_list = []
-        for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
-            td_tags = tr_tag.find_all("td")
-            for td_tag in td_tags:
-                try:
-                    temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
-                except:
-                    temp_list.append("")
-        new_planet_data.append(temp_list)
-    except:
-        time.sleep(1)
-        scrape_more_data(hyperlink)
-scrape()
-for index, data in enumerate(planet_data):
-    scrape_more_data(data[5])
-    print(f"{index+1} page done 2")
-final_planet_data = []
-for index, data in enumerate(planet_data):
-    new_planet_data_element = new_planet_data[index]
-    new_planet_data_element = [elem.replace("\n", "") for elem in new_planet_data_element]
-    new_planet_data_element = new_planet_data_element[:7]
-    final_planet_data.append(data + new_planet_data_element)
+    //CODE
+
 with open("final.csv", "w") as f:
         csvwriter = csv.writer(f)
         csvwriter.writerow(headers)
